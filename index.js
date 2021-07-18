52d1392fd5c7c84d16690b24ccfd98238fcac357
tdev88 <tracy.dev@outlook.com>
2021-07-18 22:19:54 +1000

    update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(satActivity='roller-skate') {
+  return(`This Saturday I want to ${satActivity}!`);
+}
+
+function mondayWork(monActivity='go to office') {
+  return (`This Monday, I will ${monActivity}`)
+}
+
+function wrapAdjective(flair='*') {
+  return function (compliment='special') {
+    return (`${flair}You are ${compliment}${flair}`)
+  }
+}
